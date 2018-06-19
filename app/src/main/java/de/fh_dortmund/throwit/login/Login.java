a83e3691e2f78db93be7d96bdc7450b22246b386
Joschi <joschi_mehta@gmx.de>
2018-06-19 15:52:57 +0200

    Registration complete? Cannot test on emulator

--- app/src/main/java/de/fh_dortmund/throwit/login/Login.java
+++ app/src/main/java/de/fh_dortmund/throwit/login/Login.java
@@ -1,5 +1,6 @@
 package de.fh_dortmund.throwit.login;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import de.fh_dortmund.throwit.R;
 import de.fh_dortmund.throwit.menu.Menu;
@@ -41,6 +47,51 @@ public class Login extends AppCompatActivity {
             public void onClick(View v){
                 Intent registerIntent = new Intent(Login.this, RegisterActivity.class);
                 Login.this.startActivity(registerIntent);
+            }
+        });
+
+
+        bLogin.setOnClickListener(new View.onClickListener() {
+            @Override
+            public void onClick(View v){
+
+                final String user = username.getText().toString();
+                final String pass = password.getText().toString();
+
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if(success){
+                                // send to main activity
+
+
+                                // Intent intent = new Intent(Login.this, // Klasse einfügen);
+
+                                // Login.this.startActivity(intent);
+                            }
+                            else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
+                                builder.setMessage("Login failded")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                LoginRequest loginRequest = new LoginRequest(user, pass, responseListener);
             }
         });
     }
