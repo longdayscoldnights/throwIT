361f1f291e9317fb75b81a985441a67f197f5036
Joschi <joschi_mehta@gmx.de>
2018-07-08 13:52:23 +0200

    Using Google Firebase for Login and Register Process with E-Mail and Password. Also give some error messages

--- app/src/main/java/de/fh_dortmund/throwit/login/Login.java
+++ app/src/main/java/de/fh_dortmund/throwit/login/Login.java
@@ -1,19 +1,46 @@
 package de.fh_dortmund.throwit.login;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import de.fh_dortmund.throwit.R;
+import de.fh_dortmund.throwit.menu.DeviceInformation;
 import de.fh_dortmund.throwit.menu.Menu;
 
 /**
  * @author Bijan Riesenberg
  */
 public class Login extends AppCompatActivity {
+
+    private EditText etEmail;
+    private EditText etPass;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private final String TAG = "Sign in";
 
 
     @Override
@@ -22,12 +49,34 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
-        final EditText username = (EditText) findViewById(R.id.edit_username);
-        final EditText password = (EditText) findViewById(R.id.edit_password);
+        etEmail = (EditText) findViewById(R.id.edit_mail);
+        etPass = (EditText) findViewById(R.id.edit_password);
         final Button bLogin = (Button) findViewById(R.id.btn_login);
         final Button registerLink = (Button) findViewById(R.id.btn_register);
+        final Button play = (Button) findViewById(R.id.no_user);
+        final Button device = (Button) findViewById(R.id.device);
 
 
+        mAuth = FirebaseAuth.getInstance(); // Get reference to the Firebase auth object
+
+
+        // Listener to detect sign in and out
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if(user != null){
+                    Log.d(TAG, "Signed in: " + user.getUid());
+                }
+                else {
+                    Log.d(TAG, "Currently signed out");
+                }
+            }
+        };
+
+
+/*
         registerLink.setOnClickListener(new View.OnClickListener(){ // Creating a listener which leads you from the start page to the register page
             @Override
             public void onClick(View v){
@@ -35,57 +84,39 @@ public class Login extends AppCompatActivity {
                 Login.this.startActivity(registerIntent);
             }
         });
+*/
+
+        registerLink.setOnClickListener(new View.OnClickListener(){ // Creating a listener which leads you from the start page to the register page
+            @Override
+            public void onClick(View v){
+               createUserAccount();
+            }
+        });
 
 
         bLogin.setOnClickListener(new View.OnClickListener() {
-
             @Override
-            public void onClick(View v){
-
-                //final String user = username.getText().toString();
-                //  final String pass = password.getText().toString();
-                launchMenu();
-
-
-                /*  Response.Listener<String> responseListener = new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-
-                        try {
-                            JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
-
-                            //TODO REMOVE LATER
-                            success = true;
-                            if(success){
-                                // send to main activity
-
-
-                                // Intent intent = new Intent(Login.this, // Klasse einfügen);
-
-                                // Login.this.startActivity(intent);
-                            }
-                            else {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
-                                builder.setMessage("Login failded")
-                                        .setNegativeButton("Retry", null)
-                                        .create()
-                                        .show();
-                            }
-
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-                }; */
-
-                // LoginRequest loginRequest = new LoginRequest(user, pass, responseListener);
+            public void onClick(View v) {
+                signUserIn();
             }
         });
-    }
 
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUserIn();
+            }
+        });
+
+        device.setOnClickListener(new View.OnClickListener() { // Getting device information
+           @Override
+           public void onClick(View v){
+               DeviceInformation d = new DeviceInformation();
+               Log.i("device: ",d.getDeviceName());
+           }
+        });
+
+    }
 
     /**
      * Send Intent to Start Menu Activity.
@@ -95,4 +126,137 @@ public class Login extends AppCompatActivity {
         startActivity(intent);
     }
 
-}
+
+
+    /**
+     *  Add and remove Listener
+     */
+    @Override
+    public void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop (){
+        super.onStop();
+
+        if(mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void updateStatus(){
+       // launchMenu();
+        TextView status = (TextView) findViewById(R.id.tvStatus);
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if(user != null){
+            status.setText("Signed in" + user.getEmail());
+        }
+        else {
+            status.setText("Signed out");
+        }
+
+    }
+
+    /**
+     *  Method to create an User Account and give a toast if the creation was successful
+     */
+    private void createUserAccount(){
+
+      String mail = etEmail.getText().toString();
+      String password = etPass.getText().toString();
+
+      mAuth.createUserWithEmailAndPassword(mail, password)// Using CreateUserWithEmailAndPassword Method --> Google Firebase
+              .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                  @Override
+                  public void onComplete(@NonNull Task<AuthResult> task) {
+
+                     /* // Counting the number of characters of the password
+                      String password = etPass.getText().toString();
+                      int count = 0;
+                      for(int i = 0; i < password.length(); i++){
+                          count += 1;
+                      } */
+
+                      if(task.isSuccessful()){
+                            Toast.makeText(Login.this, "User was created", Toast.LENGTH_SHORT).show();
+                        }
+                       /* else if(count < 6){
+                          Toast.makeText(Login.this, "Password needs at least 6 characters", Toast.LENGTH_SHORT).show();
+
+                      } */
+                        else {
+                            Toast.makeText(Login.this, "CreationFailed", Toast.LENGTH_SHORT).show();
+                        }
+                  }
+              })
+      .addOnFailureListener(new OnFailureListener() {
+          @Override
+          public void onFailure(@NonNull Exception e) {
+              if(e instanceof FirebaseAuthUserCollisionException){
+                  updateStatus("This E-Mail is already in use");
+              }
+              else {
+                  updateStatus(e.getLocalizedMessage());
+              }
+          }
+      });
+
+    }
+
+
+    private void signUserIn(){
+        String mail = etEmail.getText().toString();
+        String password = etPass.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(mail, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Login.this, "Login successful", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(Login.this, "Login failed", Toast.LENGTH_SHORT).show();
+                        }
+                        updateStatus();
+                    }
+
+                })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if(e instanceof FirebaseAuthInvalidCredentialsException){
+                    updateStatus("Invalid password");
+                }
+                else if(e instanceof FirebaseAuthInvalidUserException){
+                    updateStatus("No Account with this E-Mail");
+                }
+                else {
+                    updateStatus(e.getLocalizedMessage());
+                }
+            }
+        });
+    }
+
+
+    private void signOut(){ // there is no sign out Button but we have the method already
+        mAuth.signOut();
+        updateStatus();
+
+    }
+
+    private void updateStatus(String status){
+        TextView tvStat = (TextView) findViewById(R.id.tvStatus);
+        tvStat.setText(status);
+    }
+
+
+
+    }
+
+
+
+
--- app/src/main/java/de/fh_dortmund/throwit/menu/AchieveFragment.java
+++ app/src/main/java/de/fh_dortmund/throwit/menu/AchieveFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import de.fh_dortmund.throwit.R;
 
@@ -54,11 +55,13 @@ public class AchieveFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
